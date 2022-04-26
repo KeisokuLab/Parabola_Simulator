4e7e1bcc2f9127871ccdc9b74ff5f165fa4e73bc
CHOUSHIN CHOU <smzhg0524@gmail.com>
2022-04-26 20:37:04 +0900

    Fixed

--- parabola.py
+++ parabola.py
@@ -7,23 +7,28 @@ x = []
 y = []
 x.append(0)
 y.append(0)
-v0 = 30.0
-theta = 35.0
+v0 = 50.0
+theta = 45.0
 g = 9.81
 
-#抗力係数
-Cd = 0.47
 #半径 m
-r = 0.05
+r = 0.005
 A = math.pi * r * r
-#動粘度
-vv = 15.12*10**-6
+
 #密度
-density = 1300
+density = 350
+density_air = 1.205
 mass = (4/3)*math.pi*r*r*r*density
 
-def DCal(Cd,u):
-    return Cd*density*u*u*A/2
+def ReCal(u):
+    return r*2*u / (1.512*10**-5)
+
+def CdCal(Re):
+    return 24/Re + (2.6*(Re/5.0))/(1+(Re/5.0)**1.52) + 0.411*((Re/(2.63*10**5))**-7.94)/(1+(Re/(2.63*10**5))**-8.0) + (0.25*(Re/10**6)) / (1+(Re/10**6))
+
+def DCal(u,Cd):
+    return 0.5 * Cd * density_air * A * u * u
+
 vx = []
 vx.append(v0 * math.cos(math.pi * theta / 180))
 vy = []
@@ -35,21 +40,25 @@ ya = 0
 vya = 0
 Dx = []
 Dy = []
-Re = 2*r*v0/vv
-Cd = 24/Re
-Dx.append(-abs(DCal(Cd,v0)*vx[0]/v0))
-Dy.append(-abs(DCal(Cd,v0)*vy[0]/v0))
 
-for i in range(300):
-    vx.append(vx[i] - Dx[i] * dt / mass)
-    vy.append(vy[i] - g * dt + Dy[i] * dt / mass)
+Re0 = ReCal(v0)
+Cd0 = CdCal(Re0)
+Dx.append(-abs(DCal(v0,Cd0))*vx[0]/v0)
+Dy.append(-abs(DCal(v0,Cd0))*vy[0]/v0)
+
+for i in range(1000):
+    vx.append(vx[i] + (Dx[i] / mass) * dt)
+    vy.append(vy[i] - g * dt + (Dy[i] / mass) * dt)
     V = math.sqrt(vx[i+1] * vx[i+1] + vy[i+1] * vy[i+1])
-    Re = 2*r*V/vv
-    Cd = 24/Re
-    Dx.append(-abs(DCal(Cd,V)*vx[i+1]/V))
-    Dy.append(-abs(DCal(Cd,V)*vy[i+1]/V))
-
-for i in range(300):
+    Re = ReCal(V)
+    Cd = CdCal(Re)
+    Dx.append(-abs(DCal(V,Cd)*vx[i+1]/V))
+    Dy.append(-abs(DCal(V,Cd)*vy[i+1]/V))
+    print(vx[i])
+    
+for i in range(1000):
+    if y[i] < 0:
+        break
     x.append(x[i] + (vx[i] + vxa)*dt/2)
     y.append(y[i] + (vy[i] + vya)*dt/2)
     xa = x[i] + dt * vx[i]
@@ -58,8 +67,6 @@ for i in range(300):
     ya = y[i] +  dt * vy[i]
     ya_next = y[i+1] + dt * vy[i+1]
     vya = (ya_next - ya) / dt
-
-print(Dx,Dy)
 
 plt.plot(x, y)
 plt.show()
